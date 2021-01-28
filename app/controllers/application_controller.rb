5be4d96773b2a661d5dc8714f78db94e5875a827
sk534 <asatish764@gmail.com>
2021-01-28 17:12:59 +0530

    Intendation check1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,9 @@ class ApplicationController < ActionController::API
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  def configure_permitted_parameters
-    added_attrs = [:username,:email, :password, :password_confirmation]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+	def configure_permitted_parameters
+		added_attrs = [:username,:email, :password, :password_confirmation]
+		devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+		devise_parameter_sanitizer.permit :account_update, keys: added_attrs
 	end
-	
 end
