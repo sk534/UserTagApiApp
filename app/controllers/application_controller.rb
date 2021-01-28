c7a2c5f5c2990816d916e98355eafc5d25ce0c29
sk534 <asatish764@gmail.com>
2021-01-28 17:16:10 +0530

    Intendation check2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::API
-	#include RailsWarden::Authentication
-	#protect_from_forgery unless: -> { request.format.json? }
-	before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-	def configure_permitted_parameters
-		added_attrs = [:username,:email, :password, :password_confirmation]
-		devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-		devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-	end
+  def configure_permitted_parameters
+    added_attrs = [:username,:email, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
